57d35322d4900715d226b9abd280655155558571
John Fleming <johndeclanfleming@gmail.com>
2020-07-29 20:09:21 -0500

    Add project files.

---
+++ MaskSensitiveInformation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace MaskSensitiveInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a secret password:");
+            var secret = Console.ReadLine();
+
+            var maskedSecret = new StringBuilder();
+
+            foreach (var c in secret)
+            {
+                var len = secret.Length;
+                var position = secret.IndexOf(c);
+                var distanceFromEnd = len - position;
+
+                if (distanceFromEnd > 4)
+                {
+                    maskedSecret.Append("*");
+                }
+                else
+                {
+                    maskedSecret.Append(c);
+                }
+            }
+            
+            Console.WriteLine(maskedSecret);
+        }
+    }
+}
